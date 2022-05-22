57174bc737d9d4a96502f0b44b65ccf00990a2fa
ckim14 <chikakim@gmail.com>
2022-05-22 18:12:35 -0400

    implement notes POST API

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,15 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const bodyParser = require("body-parser");
+const KSUID = require("ksuid");
 
 const PORT = 4001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
 
 function readNotes() {
   const noteData = fs.readFileSync(
@@ -17,9 +21,35 @@ function readNotes() {
   return JSON.parse(noteData);
 }
 
+function saveNotes(noteData) {
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(noteData),
+    "utf-8"
+  );
+}
+
 // return the notes saved in db.json
 app.get("/api/notes", (req, res) => {
   res.json(readNotes());
+});
+
+// validate the request and add the new note to db.json
+app.post("/api/notes", (req, res) => {
+  if (!req.body || !req.body.title || !req.body.text) {
+    res.status(400).send("missing required fields title or text");
+    return;
+  }
+
+  const notesData = readNotes();
+
+  const newNote = { id: `${KSUID.randomSync().string}`, ...req.body };
+
+  notesData.push(newNote);
+
+  saveNotes(notesData);
+
+  res.status(201).json(newNote);
 });
 
 app.get("/notes", (req, res) => {
